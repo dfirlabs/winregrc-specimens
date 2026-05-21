c2829e579145af195c135304cc360c11d471cf43
Joachim Metz <joachim.metz@gmail.com>
2026-05-21 15:01:24 +0200

    Worked on script to generate specimens

--- generate-specimens-windows.ps1
+++ generate-specimens-windows.ps1
@@ -1,19 +1,21 @@
 # Script to generate Windows Registry resources test data
 
+# Open notepad via the Windows shell
+
 $WindowsShell = New-Object -ComObject "wscript.shell"
 
-New-Item -ItemType "File" -Name "text.txt"
+New-Item -ItemType "File" -Name "text1.txt" -Force | Out-Null
 
-$WindowsShell.Run("write.exe ${Pwd}\text.txt")
+$WindowsShell.Run("notepad.exe ${Pwd}\text1.txt")
 
-# Bring the window named "WordPad" in focus
-$WindowsShell.AppActivate("text.txt - WordPad")
+# Bring the window named "Notepad" in focus
+$WindowsShell.AppActivate("text1.txt - Notepad")
 
 Start-Sleep -Seconds 2.0
 
 # $WindowsShell.SendKeys("%Y")
 
-$WindowsShell.SendKeys("First line of text")
+$WindowsShell.SendKeys("First line of text in notepad")
 $WindowsShell.SendKeys("{ENTER}")
 $WindowsShell.SendKeys("Some more text")
 
@@ -24,9 +26,81 @@ Start-Sleep -Seconds 1.0
 
 $WindowsShell.SendKeys("%S")
 
-# TODO: experiment with different focus durations
+# Open notepad via its Start Menu shortcut with the Windows shell
 
-# TODO: open notepad.exe using Deskop LNK
-# TODO: open some using non-Deskop LNK
+$NotepadLnkPath = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Accessories\Notepad.lnk"
+
+$WindowsShell = New-Object -ComObject "wscript.shell"
+
+New-Item -ItemType "File" -Name "text2.txt" -Force | Out-Null
+
+# Use escaped quotes to handle spaces correctly
+$WindowsShell.Run("cmd /c start `"`" `"$NotepadLnkPath`" `"${Pwd}\text2.txt`"")
+
+$WindowsShell.AppActivate("text2.txt - Notepad")
+
+Start-Sleep -Seconds 2.0
+
+$WindowsShell.SendKeys("First line of text in notepad via shortcut")
+$WindowsShell.SendKeys("{ENTER}")
+$WindowsShell.SendKeys("Some more text")
+
+# Send Alt+F4
+$WindowsShell.SendKeys("%{F4}")
+
+Start-Sleep -Seconds 1.0
+
+$WindowsShell.SendKeys("%S")
+
 # TODO: open non exe using LNK
-# TODO: open LNK via PIDL
+
+# Open notepad via the PIDL in the Start Menu shortcut with the Windows shell
+
+$ShellApp = New-Object -ComObject "Shell.Application"
+$WindowsShell = New-Object -ComObject "wscript.shell"
+
+# %CommonPrograms% => "C:\ProgramData\Microsoft\Windows\Start Menu\Programs"
+$ProgramsPath = [Environment]::GetFolderPath("CommonPrograms")
+$AccessoriesPath = Join-Path $ProgramsPath "Accessories"
+
+if (-not (Test-Path $AccessoriesPath)) {
+    New-Item -ItemType "Directory" -Path $AccessoriesPath -Force | Out-Null
+}
+
+$LnkFilePath = Join-Path $AccessoriesPath "Notepad.lnk"
+if (-not (Test-Path $LnkFilePath)) {
+    $Shortcut = $WindowsShell.CreateShortcut($LnkFilePath)
+    $Shortcut.TargetPath = "C:\Windows\System32\notepad.exe"
+    $Shortcut.Save()
+}
+
+$ShortcutFolder = $ShellApp.NameSpace($AccessoriesPath)
+$ShortcutItem = $ShortcutFolder.ParseName("Notepad.lnk")
+
+# InvokeVerb("open") evaluates the internal tracking structure (PIDL stream metadata) 
+# This emulates double-clicking an explorer item
+$ShortcutItem.InvokeVerb("open")
+
+Start-Sleep -Seconds 1.5
+
+New-Item -ItemType "File" -Name "text3.txt" -Force | Out-Null
+
+$WindowsShell.AppActivate("Untitled - Notepad")
+
+$WindowsShell.SendKeys("First line of text in notepad via PIDL")
+$WindowsShell.SendKeys("{ENTER}")
+$WindowsShell.SendKeys("Some more text")
+
+# Send Ctrl+S
+$WindowsShell.SendKeys("^s")
+Start-Sleep -Seconds 3.0
+
+$WindowsShell.SendKeys("${Pwd}\text3.txt")
+$WindowsShell.SendKeys("{ENTER}")
+Start-Sleep -Seconds 1.0
+
+# Send Alt+F4
+$WindowsShell.SendKeys("%{F4}")
+
+# Prevent "Error: The operation was canceled."
+Get-Process -ErrorAction SilentlyContinue -Name "notepad" | Stop-Process -Force
--- prepare-specimens-windows.ps1
+++
@@ -1,9 +0,0 @@
-# Script to prepare for generating Windows Registry resources test data
-
-Remove-Item -ErrorAction SilentlyContinue -Recurse -Path "HKEY_CURRENT_USER\Software\Microsoft\Windows\Currentversion\Explorer\RecentDocs\"
-Remove-Item -ErrorAction SilentlyContinue -Recurse -Path "HKEY_CURRENT_USER\Software\Microsoft\Windows\Currentversion\Explorer\TypedPaths\"
-Remove-Item -ErrorAction SilentlyContinue -Recurse -Path "HKEY_CURRENT_USER\Software\Microsoft\Windows\Currentversion\Explorer\UserAssist\"
-Remove-Item -ErrorAction SilentlyContinue -Recurse -Path "HKEY_CURRENT_USER\Software\Microsoft\Windows\Currentversion\Explorer\WordWheelQuery\"
-
-Remove-Item -ErrorAction SilentlyContinue -Recurse -Path "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\Shell\BagMRU"
-Remove-Item -ErrorAction SilentlyContinue -Recurse -Path "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\Shell\Bags"
